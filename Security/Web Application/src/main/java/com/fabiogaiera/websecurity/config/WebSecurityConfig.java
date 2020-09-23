774424ccacc5ac40184093a3a5d7fe732f35a25c
Fabio Gaiera <fagaiera@gmail.com>
2020-09-23 20:47:52 +0200

    Security

--- Security/Web Application/src/main/java/com/fabiogaiera/websecurity/config/WebSecurityConfig.java
+++ Security/Web Application/src/main/java/com/fabiogaiera/websecurity/config/WebSecurityConfig.java
@@ -13,32 +13,37 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
-	}
 
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-		return new InMemoryUserDetailsManager(user);
-	}
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+
+    }
 
 }
